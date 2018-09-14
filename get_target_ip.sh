526a8d30756ac286a3e53ba9f9fd7be93c766477
sebglauser <sebastien.glauser@he-arc.ch>
2018-09-14 10:15:21 +0200

    fix: target not found

--- get_target_ip.sh
+++ get_target_ip.sh
@@ -6,7 +6,8 @@ targets_ip=".targets_ip"
 
 sudo ls > /dev/null 
 
-res_nmap="$(sudo nmap -sP -n 157.26.100.0/24)"
+res_nmap=$(cat scan)
+#res_nmap="$(sudo nmap -sP -n 157.26.100.0/24)"
 echo "Nmap done..."
 
 macs="$(sed "/^#/d" $targets | awk '{print $2}')"
@@ -14,9 +15,17 @@ macs="$(sed "/^#/d" $targets | awk '{print $2}')"
 [[ -f $ip ]] && rm $ip
 
 for mac in $macs; do
-	echo "${res_nmap}" | awk -v n=2 -v p="$mac" '$0~p && NR>n {print window[(NR-n)%n]}{window[NR%n]=$5}' >> $ip
+	[ ${#mac} -ne 17 ] && continue
+	if echo $res_nmap | grep $mac >> /dev/null; then
+		echo "${res_nmap}" | awk -v n=2 -v p="$mac" '$0~p && NR>n {print w[(NR-n)%n]}{w[NR%n]=$5}'  >> $ip 
+	else
+		echo "not found" >> $ip
+	fi
 done 
 
 paste $targets $ip > $targets_ip
-#at $targets_pi
+sleep 0.1
 
+cat $targets_ip
+
+exit 0 
