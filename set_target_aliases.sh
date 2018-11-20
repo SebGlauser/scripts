98f14e21b884e07109bdc0f58296a0c2e5968c0a
sebglauser <sebastien.glauser@he-arc.ch>
2018-11-20 15:46:12 +0100

    changes: dont do aliases for target that is not founded

--- set_target_aliases.sh
+++ set_target_aliases.sh
@@ -4,7 +4,11 @@ targets_ip=".targets_ip"
 
 mkdir -p ~/.config;
 
-sed "/^#/d" $targets_ip | awk '{print "alias ssh"$1"=\"ssh "$3"@"$4"\""}' | tee ~/.config/targets_alias
+sed "/^#/d" $targets_ip | sed "/not found/d" | awk '{print "alias ssh"$1"=\"ssh "$3"@"$4"\""}' | tee ~/.config/targets_alias
 
 echo ""
 echo "Make sure to add source ~/.config/targets_alias in your bashrc or zshrc"
+
+sleep 0.2
+
+source ~/.config/targets_alias
